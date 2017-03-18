56bd0f4b628ed3d32308b4118bbb3ce9d3a6a638
amjones503 <amjones503@tamu.edu>
2017-03-18 02:52:54 +0000

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313220223) do
+ActiveRecord::Schema.define(version: 20170318011341) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "name"
@@ -28,7 +28,14 @@ ActiveRecord::Schema.define(version: 20170313220223) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "instructor_id"
-    t.decimal  "student_id"
+    t.integer  "student_id"
+  end
+
+  create_table "courses_students", force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "student_id"
+    t.index ["course_id"], name: "index_courses_students_on_course_id"
+    t.index ["student_id"], name: "index_courses_students_on_student_id"
   end
 
   create_table "instructors", force: :cascade do |t|
@@ -61,7 +68,7 @@ ActiveRecord::Schema.define(version: 20170313220223) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.decimal  "course_id"
+    t.integer  "course_id"
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
