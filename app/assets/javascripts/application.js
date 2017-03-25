b0a1af89179bfa037128ce266b12db00bc2f2be3
froob <froob@tamu.edu>
2017-03-25 03:06:55 +0000

    Quick fix for the webpage rendering issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,18 +16,3 @@
 //= require turbolinks
 //= require_tree .
 /*global $*/
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
-});
-
-$( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
-});
-});
