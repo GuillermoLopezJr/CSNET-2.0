6e73a7baa9599146d076ea2227d6e95e59ecb6d9
guillermo-lopez6988 <guillermo_lopez6988@tamu.edu>
2017-03-28 21:14:47 -0500

    Update roster_spec.rb

--- spec/models/roster_spec.rb
+++ spec/models/roster_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Roster, type: :model do
 
   it "has a valid course number" do
     course = FactoryGirl.build(:course)
-    roster = Roster.create!(course_num: course.number, attachment: nil)
+    roster = Roster.create!(course_num: course.number, attachment: "test.xls")
     expect(Roster.find_by(course_num: course.number)).to be_truthy
   end
 
