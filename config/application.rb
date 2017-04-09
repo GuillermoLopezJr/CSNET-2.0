3eb5a319002317fec893a3b9c4329fed8e877279
froob <froob@tamu.edu>
2017-04-09 20:12:24 +0000

    fixed submission time to central

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module AggiesOffTheRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-   config.time_zone = Time.now.zone
-
+   config.time_zone = 'Central Time (US & Canada)'
+   config.active_record.default_timezone = :local # Or :utc
     
   end
 end
